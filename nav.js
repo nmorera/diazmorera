03e912f4d2d08d097f6b4335e346dd6025c1ff12
Nestor Diaz Morera <105933075+nmorera@users.noreply.github.com>
2024-08-12 15:45:55 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ document.write('<nav><ul>\
 			<li> <a href="./index.html">Home</a></li>\
 			<li> <a href="./research.html">Research</a></li>\
 			<li> <a href="./teaching.html">Teaching</a></li>\
-			<li> <a href="./service.html">Links</a></li>\
-			<li> <a href="./writing.html">Code</a></li>\
-			<li> <a href="./fun.html">Posts</a></li>\
+			<li> <a href="./links.html">Links</a></li>\
+			<!--<li> <a href="./code.html">Code</a></li>\-->
+			<!--<li> <a href="./post.html">Posts</a></li>\-->
 </ul></nav>');
