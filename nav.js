a5f54ccb25a55fd32b5ecd4f164d24d42d3cb24b
Nestor Diaz Morera <105933075+nmorera@users.noreply.github.com>
2024-08-13 14:05:33 -0500

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-document.write('<nav><ul>\
-			<li> <a href="./index.html">Home</a></li>\
-			<li> <a href="./research.html">Research</a></li>\
-			<li> <a href="./teaching.html">Teaching</a></li>\
-			<li> <a href="./service.html">Links</a></li>\
-			<li> <a href="./writing.html">Code</a></li>\
-			<li> <a href="./fun.html">Posts</a></li>\
-</ul></nav>');
